a8da7a3f2b3345ab71a6b02d9f863e58c07f0ee9
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-04 14:17:06 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+
+  def breed=(dog_breed)
+    @this_dogs_breed = dog_breed
+  end
+
+  def breed
+    @this_dogs_breed
+  end
+
+end
